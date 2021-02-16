af178c55383026916de30d6eb3f6f079032ab001
A8H1K <abhikchoudhary2007@gmail.com>
2021-02-16 11:15:37 +0530

    fonts

--- dennisivy/settings.py
+++ dennisivy/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('key.txt') as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = 'hrj48*mobkzo)s6q+2wfqcg=_=@5c38n&sb4_vlp#bb^zyp_pi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'False'
